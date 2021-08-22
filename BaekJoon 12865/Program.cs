508ac3db24760dcaa0fcdb6829c98595a4309b91
PassingG <49996889+PassingG@users.noreply.github.com>
2021-08-22 18:12:42 +0900

    Initial commit

---
+++ BaekJoon 12865/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] travelObject = new int[,] {{4,7}, {6,13}, {4,8}, {3,6}, {5,12}};
+
+            int result = Solve(travelObject);
+
+            Console.WriteLine($"{result}");
+        }
+
+        private static int Solve(int[,] travelObject)
+        {
+            int count = 0;
+            
+            for()
+            {
+
+            }
+
+            return count;
+        }
+    }
+}
---
+++ 음료수얼려먹기/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace 음료수얼려먹기
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputLine = System.Console.ReadLine();
+
+            string[] split = inputLine.Split(" ");
+
+            if(split.Length != 2) return;
+        }
+
+        private static int Solution(int height, int width, int[,] iceTray)
+        {
+            int howManyIcecream = 0;
+
+
+        }
+    }
+}

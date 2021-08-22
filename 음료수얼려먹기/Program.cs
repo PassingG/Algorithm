31b2236accb23e752aabc7448e3623b719edf994
PassingG <49996889+PassingG@users.noreply.github.com>
2021-08-22 21:25:29 +0900

    Question 음료수얼려먹기
    Question 음료수얼려먹기

--- 음료수얼려먹기/Program.cs
+++ 음료수얼려먹기/Program.cs
@@ -6,18 +6,97 @@ namespace 음료수얼려먹기
     {
         static void Main(string[] args)
         {
+            Solve solve = new Solve();
+
+            solve.Init();
+            solve.Calculate();
+            solve.Log();
+        }
+    }
+
+    public class Solve
+    {
+        int width = 0;
+        int height = 0;
+
+        int[,] iceTray;
+        int howManyIcecream = 0;
+
+        public void Init()
+        {
+            // Input Height, Width
             string inputLine = System.Console.ReadLine();
 
             string[] split = inputLine.Split(" ");
 
-            if(split.Length != 2) return;
+            if (split.Length != 2)
+            {
+                Console.WriteLine("Wrong input");
+                return;
+            }
+
+            height = Int32.Parse(split[0]);
+            width = Int32.Parse(split[1]);
+
+            iceTray = new int[height, width];
+
+            // Input icetray
+            for (int x = 0; x < height; x++)
+            {
+                inputLine = System.Console.ReadLine();
+
+                char[] splitArray = inputLine.ToCharArray();
+
+                if (splitArray.Length != width)
+                {
+                    Console.WriteLine($"Wrong input {splitArray.Length} {width}");
+                    return;
+                }
+
+                for (int y = 0; y < width; y++)
+                {
+                    iceTray[x, y] = (int)Char.GetNumericValue(splitArray[y]); ;
+                }
+            }
         }
 
-        private static int Solution(int height, int width, int[,] iceTray)
+        public void Calculate()
         {
-            int howManyIcecream = 0;
+            for (int x = 0; x < height; x++)
+            {
+                for (int y = 0; y < width; y++)
+                {
+                    if (DFS(x, y))
+                    {
+                        howManyIcecream++;
+                    }
+                }
+            }
+        }
 
+        public bool DFS(int x, int y)
+        {
+            if(x < 0 || x >= height || y < 0 || y >= width)
+            {
+                return false;
+            }
 
+            if(iceTray[x,y] == 0)
+            {
+                iceTray[x, y] = 1;
+
+                DFS(x + 1, y);
+                DFS(x - 1, y);
+                DFS(x, y + 1);
+                DFS(x, y - 1);
+                return true;
+            }
+            return false;
+        }
+
+        public void Log()
+        {
+            Console.WriteLine($"Result : {howManyIcecream}");
         }
     }
 }
